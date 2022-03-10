24e7f5241016771f3796e773cc02d46a8d1d47fa
talentlessDeveloper <kareemope52@gmail.com>
2022-03-10 15:07:41 +0100

    Updates

---
+++ static/js/871.8dad7f81.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkeden=self.webpackChunkeden||[]).push([[871],{871:function(e,s,n){n.r(s);n(2791);var t=n(5225),u=n(184);s.default=function(){return(0,u.jsx)("div",{children:(0,u.jsx)(t.default,{title:"The wall of \ud83d\udc9a",className:"food-customers"})})}}}]);
+//# sourceMappingURL=871.8dad7f81.chunk.js.map
---
+++ static/js/877.36124275.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkeden=self.webpackChunkeden||[]).push([[877],{3877:function(e,t,s){s.r(t);s(2791);var n=s(5225),c=s(184);t.default=function(){return(0,c.jsxs)("div",{className:"companyCustomer",children:[(0,c.jsx)(n.default,{title:"The wall of \ud83d\udc9a"}),(0,c.jsx)("button",{className:"customer-perk",children:"Get the Perfect Perk"})]})}}}]);
+//# sourceMappingURL=877.36124275.chunk.js.map
